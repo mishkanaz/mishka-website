765980a8335162340884e7a325d78c444d99ba4d
Mishka Nazarani <mishkanazarani@gmail.com>
2026-09-09 16:38:41 -0700

    minor changes

--- doodles.js
+++ doodles.js
@@ -50,7 +50,7 @@ async function loadDoodles() {
 
          const name = document.createElement("p");
 
-    name.textContent = `created by: ${doodle.username}`;
+    name.textContent = `created by: ${doodle.username ?? anonymous}`;
 
     name.classList.add(
         "text-white",
